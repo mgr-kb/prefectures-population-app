6c9f943eb179f68128a5500f38a7553ae538f600
mgr-kb <koumgrars13@gmail.com>
2025-02-05 21:19:08 +0900

    populationCompositionPerYearの取得処理を追加

---
+++ src/api/population.ts
@@ -0,0 +1,51 @@
+import { configs } from "@/configs";
+
+type PopulationData = {
+  /** ラベル */
+  label: string;
+  data: {
+    /** 年 */
+    year: number;
+    /** 人口 */
+    value: number;
+    /** 割合 */
+    rate: number;
+  }[];
+};
+
+type PopulationCompositionPerYear = {
+  /** 実績値と推計値の区切り年 */
+  boundaryYear: number;
+  data: [
+    {
+      label: "総人口";
+      data: Omit<PopulationData["data"][number], "rate">[];
+    },
+    PopulationData & { label: "年少人口" },
+    PopulationData & { label: "生産年齢人口" },
+    PopulationData & { label: "老年人口" },
+  ];
+};
+
+type Population = {
+  message: string | null;
+  result: PopulationCompositionPerYear;
+};
+
+const { baseUrl, populationCompositionPerYear } = configs;
+
+export const fetchPopulationByPrefecture = async (
+  prefCode: string,
+): Promise<Population> => {
+  const params = new URLSearchParams({ prefCode });
+  const res = await fetch(
+    `${baseUrl}${populationCompositionPerYear.path}?${params.toString()}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const data = await res.json();
+  return data;
+};
