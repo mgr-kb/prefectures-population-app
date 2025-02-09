5a50377fdf360dcc06d80c7bb9cfe7c08bca963d
mgr-kb <koumgrars13@gmail.com>
2025-02-09 16:17:36 +0900

    cursor-pointerを付与

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -6,7 +6,7 @@ type Props = ComponentPropsWithRef<"input">;
 export const Checkbox = forwardRef<HTMLInputElement, Props>(
   function CheckboxBase({ className, ...props }, ref) {
     const classes = cn(
-      "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2",
+      "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm cursor-pointer focus:ring-blue-500 focus:ring-2",
       className,
     );
     return <input type="checkbox" {...props} ref={ref} className={classes} />;
