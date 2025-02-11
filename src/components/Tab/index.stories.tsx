c70982992ef00cc3bd1caceef8f3d7298f38232a
mgr-kb <koumgrars13@gmail.com>
2025-02-11 15:23:58 +0900

    [fix]buildエラー解消_Tabのprops修正に伴うstoriesファイルの修正

--- src/components/Tab/index.stories.tsx
+++ src/components/Tab/index.stories.tsx
@@ -14,18 +14,18 @@ export const Default: StoryObj = {
       <Tab.Title>Tab Component</Tab.Title>
       <Tab.List
         tabItems={[
-          { id: 0, content: "Tab 1" },
-          { id: 1, content: "Tab 2" },
-          { id: 2, content: "Tab 3" },
-          { id: 3, content: "Tab 4" },
+          { id: "0", content: "Tab 1" },
+          { id: "1", content: "Tab 2" },
+          { id: "2", content: "Tab 3" },
+          { id: "3", content: "Tab 4" },
         ]}
       />
       <Tab.PanelList
         panelItems={[
-          { id: 0, content: "Content 1" },
-          { id: 1, content: "Content 2" },
-          { id: 2, content: "Content 3" },
-          { id: 3, content: "Content 4" },
+          { id: "0", content: "Content 1" },
+          { id: "1", content: "Content 2" },
+          { id: "2", content: "Content 3" },
+          { id: "3", content: "Content 4" },
         ]}
       />
     </Tab.Group>
